f0f5d3099f1945d2c8cfd06eb29e2c875e2999da
KhenFalcon <chiquen415@gmail.com>
2025-12-07 18:46:36 -0500

    Add files via upload
    Signed-off-by: KhenFalcon <chiquen415@gmail.com>

---
+++ app/--components/Footer.tsx
@@ -0,0 +1,20 @@
+export default function Footer() {
+    return (
+        <div id="footer">
+            <p>overhead footer text</p>
+            <hr />
+            <div id="spaced-footer">
+                <ul>
+                    <li>left footer 1</li>
+                    <li>left footer 2</li>
+                </ul>
+                <ul>
+                    <li>right footer 1</li>
+                    <li>right footer 2</li>
+                </ul>
+            </div>
+            <hr />
+            <p>ending footer text</p>
+        </div>
+    );
+}
